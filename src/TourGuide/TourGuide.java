8bd7bdb55e29351518135d9c4ed1049402f783c0
lytwaytLaz <laszlo.hago@gmail.com>
2017-08-25 10:00:03 +0200

    Testing all possible permutations. Some corner case gets miscalculated

--- src/TourGuide/TourGuide.java
+++ src/TourGuide/TourGuide.java
@@ -8,44 +8,74 @@ import java.util.Scanner;
  */
 public class TourGuide {
 
-    static int tourists;
-    static double speedGuide, speedTourist, angle, guideDistance, xT, yT, returnDistance, totalTime,
-            sumTimeChase, xG, yG;
+    static int numOfTourists;
+    static double speedGuide, speedTourist, angle, guideDistance, xT, yT, totalTime, totalTimeMin, xG, yG;
     static double[] timeChase, timeReturn;
+    static double[][] tourists;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            tourists = sc.nextInt();
-            if (tourists == 0)
+            numOfTourists = sc.nextInt();
+            if (numOfTourists == 0)
                 break;
 
             speedGuide = sc.nextDouble();
-            timeChase = new double[tourists];
-            timeReturn = new double[tourists];
-            sumTimeChase = 0;
-            xG = 0;
-            yG = 0;
+            tourists = new double[numOfTourists][4];
+            totalTimeMin = 1000000;
 
-            for (int i = 0; i < tourists; i++) {
-                xT = sc.nextDouble();
-                yT = sc.nextDouble();
-                speedTourist = sc.nextDouble();
-                angle = sc.nextDouble();
-
-                calcTourist(i);
+            for (int i = 0; i < numOfTourists; i++) {
+                tourists[i][0] = sc.nextDouble();
+                tourists[i][1] = sc.nextDouble();
+                tourists[i][2] = sc.nextDouble();
+                tourists[i][3] = sc.nextDouble();
             }
-            System.out.println(calculateTime());
+            permute(tourists, 0);
+            System.out.println(Math.round(totalTimeMin));
         }
     }
 
+
+    static void permute(double[][] a, int k) {
+        timeChase = new double[numOfTourists];
+        timeReturn = new double[numOfTourists];
+        xG = 0;
+        yG = 0;
+        if (k == a.length) {
+            for (int i = 0; i < a.length; i++) {
+                tourists[i] = a[i];
+            }
+            for (int t = 0; t < numOfTourists; t++) {
+                xT = tourists[t][0];
+                yT = tourists[t][1];
+                speedTourist = tourists[t][2];
+                angle = tourists[t][3];
+
+                calcTourist(t);
+            }
+            double time = calculateTime();
+            totalTimeMin = totalTimeMin > time ? time : totalTimeMin;
+        } else {
+            for (int i = k; i < a.length; i++) {
+                double[] temp = a[k];
+                a[k] = a[i];
+                a[i] = temp;
+
+                permute(a, k + 1);
+
+                temp = a[k];
+                a[k] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
 
     public static void calcTourist(int tourist) {
         double guessTime = 0;
         double xOrig = xT;
         double yOrig = yT;
-        double trackMax = 1000000;
+        double trackMax = 10000000;
         double trackMin = 0;
 
 //        REMOVE!!
@@ -56,51 +86,36 @@ public class TourGuide {
             yT = yOrig + Math.sin(angle) * guessTime * speedTourist;
             guideDistance = Math.sqrt((xT - xG) * (xT - xG) + (yT - yG) * (yT - yG));
 
-//            //REMOVE!!!
+////            //REMOVE!!!
 //            double temp = guideDistance / guessTime;
 //            System.out.println(c + ": " + temp);
 //            c++;
 
-            if (speedGuide > guideDistance / guessTime + 0.00001) {
+            if (speedGuide > guideDistance / guessTime + 0.000001) {
                 trackMax = guessTime;
                 guessTime -= (trackMax - trackMin) / 2;
-            } else if (speedGuide < guideDistance / guessTime - 0.00001) {
+            } else if (speedGuide < guideDistance / guessTime - 0.000001) {
                 trackMin = guessTime;
                 guessTime += (trackMax - trackMin) / 2;
             } else
                 break;
         }
-//        timeChase[tourist] = guessTime - sumTimeChase;
-//        sumTimeChase = guessTime;
-
-
         timeChase[tourist] = guessTime;
-//        returnDistance = Math.sqrt(xT * xT + yT * yT);
         timeReturn[tourist] = Math.sqrt(xT * xT + yT * yT) / speedTourist;
 
         xG = xT;
         yG = yT;
     }
 
-    private static int calculateTime() {
-        for(double k : timeChase) {
-            System.out.print(k + " ");
-            sumTimeChase += k;
-        }
-        System.out.println();
-        for(double j : timeReturn)
-            System.out.print(j+" ");
-        System.out.println();
-
-        totalTime = sumTimeChase + timeReturn[timeReturn.length - 1];
+    private static double calculateTime() {
+        totalTime = 0;
         double timeCount = 0;
         for (int i = 0; i < timeReturn.length; i++) {
             timeCount += timeChase[i];
-            if (timeReturn[i] >= totalTime - timeCount) {
+            if (timeReturn[i] > totalTime - timeCount) {
                 totalTime = timeReturn[i] + timeCount;
             }
         }
-        System.out.println(totalTime);
-        return (int)Math.round(totalTime);
+        return totalTime;
     }
 }
